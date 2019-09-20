5c4983882226c46084085bff6fe75beb99325fb7
Adrian Garcia <adriangarlop@hotmail.com>
2019-09-19 23:45:06 -0500

    Added index.js to commit

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
 function main() {
 
-	var todo = document.getElementById("listOfTodos");
+	let submit = document.getElementById("submitButton");
+	let clear = document.getElementById("clearButton");
+	let mark = document.getElementById("markAllButton");
+	let del = document.getElementById("deleteButton");
 
-	var sumbit = document.getElementById("submitButton");
-	var clear = document.getElementById("clearButton");
-	var mark = document.getElementById("markAllButton");
-	var del = document.getElementById("deleteButton");
+	let todo = document.getElementById("listOfTodos");
 
-	sumbit.addEventListener("click", (event) => {
-	    
-	    event.preventDefault();
+	submit.addEventListener("click", (event) => {
+		
+		event.preventDefault();
+		
+		let content = document.getElementById("newTodo").value;
+		
+		if (content === "")
+			return;
 
-	    let cont = document.getElementById("newTodo").value;
-
-	    let divNode;
-		let spanNode;
-		let inputNode;
-		let textNode;
-
-		if (cont == "")
-			return; 
-
-		div = document.createElement("DIV");
-		span = document.createElement("SPAN");
-		text = document.createTextNode(cont);
-		input = document.createElement("INPUT");
+		let div = document.createElement("div");
+		let span = document.createElement("span");
+		let text = document.createTextNode(content);
+		let checkbox = document.createElement("input");
 
 		div.setAttribute("class", "todo");
 		span.setAttribute("class", "checkboxText");
-		input.setAttribute("type", "checkbox");
-		input.setAttribute("class", "checkBox");
+		checkbox.setAttribute("type", "checkbox");
+		checkbox.setAttribute("class", "checkbox");
 
-		div.appendChild(input);
 		div.appendChild(span);
+		span.appendChild(checkbox);
 		span.appendChild(text);
 		todo.appendChild(div);
 	});
 
 	clear.addEventListener("click", (event) => {
 
-	    let checkBoxes = document.getElementsByClassName("checkBox");
-	    let checkBoxesKeys = Object.keys(checkBoxes);
+		event.preventDefault();
 
-	    checkBoxesKeys.forEach((i) => {
-	        checkBoxes[i].checked = false;
-	    })
+		let checkbox = document.getElementsByClassName("checkbox");
+		let checkboxKeys = Object.keys(checkbox);
+
+		checkboxKeys.forEach((i) => {
+			checkbox[i].checked = false;
+		})
 	});
 
 	mark.addEventListener("click", (event) => {
 
-		let checkBoxes = document.getElementsByClassName("checkBox");
-		let checkBoxesKeys = Object.keys(checkBoxes);
+		event.preventDefault();
 
-		checkBoxesKeys.forEach((i) => {
-			checkBoxes[i].checked = true;
+		let checkbox = document.getElementsByClassName("checkbox");
+		let checkboxKeys = Object.keys(checkbox);
+
+		checkboxKeys.forEach((i) => {
+			checkbox[i].checked = true;
 		})
 	});
 
-	del.addEventListener("click", (event) => {
+	del.addEventListener( "click", (event) => {
 
-		var list = document.getElementById("listOfTodos"); 
-		list.innerHTML = null;
+		event.preventDefault();
+
+		let list = document.getElementById("listOfTodos");
+
+		list.innerHTML = null; 
+
 	});
 }
-
 main();
